c08822e4d4eaa66a60f1c30b6945d7420cf4426b
Eric <eric.qian.d@gmail.com>
2019-06-06 22:17:46 -0700

    Logic to center cards and player info in frontend

--- client/src/components/gameplay/Hand.js
+++ client/src/components/gameplay/Hand.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import './Hand.css';
 
 class Hand extends React.Component {
   constructor(props) {
@@ -9,7 +10,7 @@ class Hand extends React.Component {
   render() {
     const {card1, card2} = this.props;
     return (
-      <div className = "Hand">
+      <div className = "Hand flex-container">
         <Card card = {card1} />
         <Card card = {card2} />
       </div>
