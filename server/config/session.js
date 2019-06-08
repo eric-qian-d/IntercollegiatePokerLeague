96409cb805522fd32e3c512e5e66993e9a5026df
Eric <eric.qian.d@gmail.com>
2019-06-08 12:56:12 -0700

    Integrated redis for sessions instead of memoryStore

--- server/config/session.js
+++ server/config/session.js
@@ -1,8 +1,9 @@
-const session = require("express-session");
-const MemoryStore = require('memorystore')(session);
-const newStore = new MemoryStore({
-  checkPeriod: 86400000 // prune expired entries every 24h
-});
+const session = require('express-session');
+const redis   = require("redis");
+const redisStore = require('connect-redis')(session);
+const client  = redis.createClient();
+// const MemoryStore = require('memorystore')(session);
+const newStore = new redisStore({ host: 'localhost', port: 6379, client: client,ttl :  260})
 
 module.exports = {
   session: session({
