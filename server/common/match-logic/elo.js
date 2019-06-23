34f01319e392d2d1b165fb7220b86429fbc92ff3
Eric <eric.qian.d@gmail.com>
2019-06-23 12:07:22 -0700

    Changed elo logic to make sure the end result is always integral

--- server/common/match-logic/elo.js
+++ server/common/match-logic/elo.js
@@ -1,4 +1,10 @@
 module.exports = {
+  /**
+   * Returns the new elo of two players in a match 
+   * @param  {Integer} winnerElo Winner's old elo
+   * @param  {Integer} loserElo Loser's old elo
+   * @return {List[Integer]}    list of new elos in the order winner, loser
+   */
   findNewElo(winnerElo, loserElo) {
     const k = 32;
     const r1 = Math.pow(10, winnerElo/400);
@@ -7,6 +13,6 @@ module.exports = {
     const e2 = r2/(r1+r2);
     const s1 = 1;
     const s2 = 0;
-    return [winnerElo + k * (s1 - e1), loserElo + k * (s2 - e2)]
+    return [parseInt(winnerElo + k * (s1 - e1), 10), parseInt(loserElo + k * (s2 - e2), 10)]
   }
 }
