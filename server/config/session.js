dfaf147ae7f824a5daf193d9ef2db6d911d5fd36
Eric <eric.qian.d@gmail.com>
2019-06-23 19:05:32 -0700

    Added redis env, shouldn't work necessarily locally because of elasticache restrictions

--- server/config/session.js
+++ server/config/session.js
@@ -1,14 +1,13 @@
 const session = require('express-session');
 const redis   = require("redis");
 const redisStore = require('connect-redis')(session);
-const client  = redis.createClient({host: 'localhost', port: 6379});
-// const MemoryStore = require('memorystore')(session);
-
+const redisHost = process.env.REDIS_HOST || 'localhost';
+const client  = redis.createClient({host: redisHost, port: 6379});
 
 const connectTimeout = 24 * 60 * 60 * 1000;
 const maxRetryTime = 3 * 60 * 1000;
 
-const newStore = new redisStore({host: 'localhost', port: 6379, connect_timeout : connectTimeout})
+const newStore = new redisStore({client: 'client', port: 6379, connect_timeout : connectTimeout})
 
 
 client.on("error", function (err)
@@ -23,9 +22,6 @@ module.exports = {
     store: newStore,
     resave: false,
     saveUninitialized: true,
-    // cookie: {
-    //     expires: 24*60*60*1000
-    // }
   }),
   MemoryStore: newStore,
 }
