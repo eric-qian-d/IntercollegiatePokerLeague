58e940153041221835d90f7c12fa9583eabc72f1
Eric <eric.qian.d@gmail.com>
2019-07-14 21:48:09 -0700

    Configure for https in frontend

--- client/src/vars.js
+++ client/src/vars.js
@@ -1,8 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
-console.log(isProduction);
 
 module.exports = {
   protocol: 'http',
   serverEndpoint: isProduction ? 'api.pokerzone.io' : 'localhost',
-  port: 8081,
+  port: isProduction ? '' : 8081,
 }
