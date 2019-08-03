09ddbd3f58a4579f413ab9a737aff9292a8fa569
Eric <eric.qian.d@gmail.com>
2019-08-02 20:58:45 -0700

    Restyled team conatiners in custom match lobby

--- client/src/components/custom-match-lobby/PlayerListing.js
+++ client/src/components/custom-match-lobby/PlayerListing.js
@@ -10,29 +10,21 @@ class PlayerListing extends React.Component {
   //eventually should make props pass in something that's not just a name
   render() {
     const {playerName, status, renderKick, socket, playerId, ownerId} = this.props;
-    const statusColorMap = {
-      'lobby': 'black',
-      'in progress': 'yellow',
-      'won': 'green',
-      'lost': 'red',
-    }
-    const style = {
-      borderStyle: 'solid',
-      borderWidth: '3px',
-      width: '200px',
-    }
-    style.borderColor = statusColorMap[status];
     if (renderKick && playerId !== ownerId) {
       return (
-        <div style = {style} className = 'CustomMatchPlayerListing'>
-          {playerName}
+        <div className = 'CustomMatchPlayerListingContainer LightGreyDiv'>
+          <div className = 'CustomMatchPlayerListingText'>
+            {playerName}
+          </div>
           <KickPlayerButton socket = {socket} playerId = {playerId}/>
         </div>
       )
     } else {
       return (
-        <div style = {style} className = 'CustomMatchPlayerListing'>
-          {playerName}
+        <div className = 'CustomMatchPlayerListingContainer LightGreyDiv'>
+          <div className = 'CustomMatchPlayerListingText'>
+            {playerName}
+          </div>
         </div>
       )
     }
