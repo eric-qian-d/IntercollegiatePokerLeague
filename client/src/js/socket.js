2cdb394a4549870d23c60103fd66250a5a6c483a
Eric <eric.qian.d@gmail.com>
2019-08-05 19:20:39 -0700

    Fixed true typo

--- client/src/js/socket.js
+++ client/src/js/socket.js
@@ -2,6 +2,6 @@ import openSocket from 'socket.io-client';
 import vars from '../vars';
 
 export const makeSocket = () => {
-  return openSocket(vars.protocol + '://' + vars.serverEndpoint + ':' + vars.port, { reconnection: truer });// to make adaptable
+  return openSocket(vars.protocol + '://' + vars.serverEndpoint + ':' + vars.port, { reconnection: true });// to make adaptable
 
 }
